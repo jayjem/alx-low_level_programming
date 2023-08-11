cfca3a7d85839920380f8f3f26ddfab93fe37104
jayjem <aimiebenomo1@gmail.com>
2023-08-11 13:58:17 -0700

    using calloc to assign memory

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,1 +1,24 @@
-ghih
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _calloc - allocates emory for an array of elements
+ * @nmemb: number of elements
+ * @size: byte size
+ * Return: pointer to newly allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *output;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	output = calloc(nmemb, size);
+	if (output == NULL)
+		return (NULL);
+	else
+		return (output);
+
+}
