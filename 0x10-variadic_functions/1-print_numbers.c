1fc01f57cd6d029ecb747e85ad4d5d449b0dc33f
jayjem <aimiebenomo1@gmail.com>
2023-08-23 16:36:13 -0700

    print numbers wih eparator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (separator != NULL)
 		{
 			printf("%d", va_arg(argument, int));
-			if  (i != (n -1))
+			if  (i != (n - 1))
 				printf("%s", separator);
 		}
 		else
