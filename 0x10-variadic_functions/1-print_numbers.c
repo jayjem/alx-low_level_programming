dbe069e8f757684519a7be3a27340541896c1f11
jayjem <aimiebenomo1@gmail.com>
2023-08-23 16:33:37 -0700

    print numbers with separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,4 +10,24 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
 
+	va_list argument;
+
+	va_start(argument, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator != NULL)
+		{
+			printf("%d", va_arg(argument, int));
+			if  (i != (n -1))
+				printf("%s", separator);
+		}
+		else
+			printf("%d", va_arg(argument, int));
+
+	}
+	putchar('\n');
+	va_end(argument);
+}
