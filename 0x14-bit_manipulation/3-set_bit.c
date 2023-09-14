c9126b124ea376b9d5cd7c786c77b098b6bd0efc
jayjem <aimiebenomo1@gmail.com>
2023-09-14 15:31:44 -0700

    setvalue of a bit to 1 at a partivulat index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,1 +1,17 @@
-shsh
+#include "main.h"
+
+/**
+ * set_bit - this function sets the value of a bit to 1
+ * when at a given index
+ * @n: this represents a pointer to the number to change
+ * @index: this is the  index of the bit to set to 1
+ * Return: 1in the case success, -1 for failure
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = ((1UL << index) | *n);
+	return (1);
+}
